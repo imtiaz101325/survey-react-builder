e13522bf122da01a7137982ec88e51a9cd9c88b6
excalliburbd <excalliburbd@gmail.com>
2017-03-15 15:01:51 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <div className="App">
+                <div className="survey">
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
